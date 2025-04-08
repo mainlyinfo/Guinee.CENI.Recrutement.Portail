a86d01346d8063d8f6eaa032ef838102ac0bd42c
mainlyinfo <harmonyakpabio@gmail.com>
2025-04-08 20:51:41 +0100

    Create guin.js

---
+++ guin.js
@@ -0,0 +1,6 @@
+
+var password = 'CENI'
+password=prompt('SMS :Bienvenue sur notre portail de recrutement en ligne, cliquez sur OK pour démarrer la candidature  ','CENI');
+if (password != 'CENI') {
+location.href='https://mainlyinfo.github.io/Guinee.CENI.Recrutement.Portail';
+}
